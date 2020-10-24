5c171be13f3857c72d3929ce7d9839b0d8da187c
samierkkila <73400985+samierkkila@users.noreply.github.com>
2020-10-24 21:00:56 +0300

    Update imageOverlay.js

--- imageOverlay.js
+++ imageOverlay.js
@@ -4,9 +4,9 @@ img = document.createElement('img');
 chrome.storage.local.get(['koekuva'],function(result){img.src=result.koekuva});
 chrome.storage.local.set({'koekuva':''},function(){console.log('filen alussa')});
 img.id='overlay';
-img.style.position = 'fixed';
+img.style.position = 'absolute';
 img.style.top = 0;
-img.style.opacity= 0.5;
+img.style.opacity= 0.3;
 document.body.appendChild(img);
                 
 
